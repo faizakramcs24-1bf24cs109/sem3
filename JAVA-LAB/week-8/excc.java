354308d09e86caad4d0c0143aacf13a7e38c79c8
faizakramcs24-1bf24cs109 <faizakram.cs24@bmsce.ac.in>
2025-12-17 20:18:39 +0530

    Update excc.java

--- JAVA-LAB/week-8/excc.java
+++ JAVA-LAB/week-8/excc.java
@@ -1,22 +1,23 @@
-// 1. Write a program that asks the user for two numbers and prints their division. Handle the case when the user enters 0 as the denominator.
+1. Write a program that asks the user for two numbers and prints their division. Handle the case when the user enters 0 as the denominator.
 
-// class q1{
-// public  q1(int a,int b){
-//     try{ 
-//     int c=a/b;
-//         System.out.println("the ans is"+c);
-//     }catch(ArithmeticException e){
-//         System.out.println("wrong cant div by zer");
-//     }
-//     finally{System.out.println("CONTINUE");}
-// }
-// }
+class q1{
+public  q1(int a,int b){
+    try{ 
+    int c=a/b;
+        System.out.println("the ans is"+c);
+    }catch(ArithmeticException e){
+        System.out.println("wrong cant div by zer");
+    }
+    finally{System.out.println("CONTINUE");}
+}
+}
 
-// class demo{
-//     public static void main(String[] args) {
-//         q1 obj=new q1(10,0);
+class demo{
+    public static void main(String[] args) {
+        q1 obj=new q1(10,0);
 
-//     }
-// }
+    }
+}
+
 
 
